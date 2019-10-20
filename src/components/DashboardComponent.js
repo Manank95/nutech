75746ae95dcb115e237807cfab21704a9ef9c1d3
Dharmik-dbcorp <dharmikbshah@gmail.com>
2019-10-20 21:24:58 +0530

    add fields in Book Test page

--- src/components/DashboardComponent.js
+++ src/components/DashboardComponent.js
@@ -8,12 +8,20 @@ class DashboardComponent extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-        email:'',
+
         fullName:'',
         contact:'',
+        email:'',
+        gender:'',
+        dob:'',
+        area:'',
+        city:'',
+        pincode:'',
+        testID:'',
         responseBackend: '',
         error: ''
-    };
+    
+      };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
@@ -30,8 +38,18 @@ class DashboardComponent extends React.Component {
             'Content-Type': 'application/json;charset=UTF-8'
         },
         data: {
+            
+            fullName: this.state.fullName,
             email: this.state.email,
-            fullName: this.state.fullName
+            contact: this.state.contact,
+            gender: this.state.gender,
+            dob: this.state.dob,
+            area: this.state.area,
+            city: this.state.city,
+            pincode: this.state.pincode,
+            testID: this.state.testID,
+            
+
         },
         withCredentials: true
     }
@@ -62,18 +80,57 @@ class DashboardComponent extends React.Component {
               <div className="col-md-3 center p-30 background-white b-r-6">
                 <h3>Register for the Lab test</h3>
                 <form className="form-transparent-grey" onSubmit={this.handleSubmit}>
+                  
+                  <div className="form-group">
+                    <label className="sr-only">TestName</label>
+                    <select value={this.state.testID} onChange={this.handleChange}>
+                      <option value="2500676"> SeroMark-1 </option>
+                      <option value="2567592"> Total Prostate Specific Antigen (PSA) </option>
+                      <option value="2567593"> Free  Prostate Specific Antigen (PSA) </option>
+                    </select>
+                  </div>
+            
+                  
                   <div className="form-group">
                     <label className="sr-only">Full Name</label>
                     <input type="text" className="form-control" placeholder="Full Name" name="fullName" value={this.state.fullName} onChange={this.handleChange} required />
                   </div>
                   <div className="form-group">
                     <label className="sr-only">Email</label>
-                    <input type="text" className="form-control" placeholder="Email" name="email" value={this.state.email} onChange={this.handleChange} required/>
+                    <input type="email" className="form-control" placeholder="Email" name="email" value={this.state.email} onChange={this.handleChange} required/>
                   </div>
                   <div className="form-group">
                     <label className="sr-only">Contact No. For communication</label>
-                    <input type="text" className="form-control" placeholder="Contact No. For communication" name="contact" value={this.state.contact} onChange={this.handleChange} required/>
+                    <input type="tel" pattern="[0-9]{10}" className="form-control" placeholder="Contact No. For communication" name="contact" value={this.state.contact} onChange={this.handleChange} required/>
                   </div>
+                  
+                  
+                  <div className="form-group">
+                    <label className="sr-only">Gender</label>
+                    <select value={this.state.gender} onChange={this.handleChange}>
+                      <option value="Male"> Male</option>
+                      <option value="Female">Female</option>
+                    </select>
+                  </div>
+
+
+                  <div className="form-group">
+                    <label className="sr-only">Date of Birth</label>
+                    <input type="date" className="form-control" placeholder="Date of Birth" name="dob" value={this.state.dob} onChange={this.handleChange} required/>
+                  </div>
+                  <div className="form-group">
+                    <label className="sr-only">Area</label>
+                    <input type="text" className="form-control" placeholder="Address" name="area" value={this.state.area} onChange={this.handleChange} required/>
+                  </div>
+                  <div className="form-group">
+                    <label className="sr-only">City</label>
+                    <input type="text" className="form-control" placeholder="City" name="city" value={this.state.city} onChange={this.handleChange} required/>
+                  </div>
+                  <div className="form-group">
+                    <label className="sr-only">Pincode</label>
+                    <input type="text" className="form-control" placeholder="Pincode" name="pincode" value={this.state.pincode} onChange={this.handleChange} required/>
+                  </div>
+                  
                   <div className="form-group">
                     <button type="submit" className="btn btn-block">Submit</button>
                   </div>
